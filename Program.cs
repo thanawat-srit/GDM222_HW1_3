f66b2b63cc68b0b38901f524546c306401f84060
Nor_Book <thanawat.srit@kmutt.ac.th>
2023-02-02 15:26:40 +0700

    Add Program

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class Program{
+    static void Main(string[] args){
+        double x = double.Parse(Console.ReadLine());
+        Console.WriteLine(f(x));
+    }
+    static double f(double x){
+        double sum = 0;
+        if(x>-1 && x<=0){
+            sum = Math.Sqrt(1-Math.Pow(x, 2));
+        }else if(x>0 && x<=1){
+            sum = -(Math.Sqrt(1-Math.Pow(x, 2)));
+        }else if(x<=-1){
+            sum = f(x+2);
+        }else if(x>1){
+            sum = f(x-2);
+        }
+        return sum;
+    }
+}
